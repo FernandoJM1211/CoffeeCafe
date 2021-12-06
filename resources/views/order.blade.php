dd189369f02925fbdc6c85266f8c7a7497cca1f9
FernandoJM1211 <fmanalu1211@gmail.com>
2021-12-06 21:00:30 +0700

    Update shop page & add order page modals

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -16,19 +16,19 @@
     <div class="table mt-8 p-2 border rounded-md bg-black bg-opacity-40 w-full text-white">
     <div class="flex flex-col divide-y-2">
         <div class="flex flex-row justify-between">
-            <div class="w-1/6 flex justify-center">00001</div>
-            <div class="w-1/6 flex justify-center">Fernando</div>
+            <div class="w-1/6 flex justify-center items-center">00001</div>
+            <div class="w-1/6 flex justify-center items-center">Fernando</div>
             <div class="w-1/6 justify-center hidden md:flex">Espresso 3x <br>Cappucino 2x <br>Arabica 2x</div>
-            <div class="w-1/6 justify-center hidden md:flex">12-12-21, 13.00</div>
+            <div class="w-1/6 justify-center hidden md:flex items-center">12-12-21, 13.00</div>
             <div class="w-1/6 flex justify-center content-start">
-                <button>
+                <button class="detail">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 p-1 rounded-lg md:border hover:bg-white hover:border-gray-400 hover:text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                     </svg> 
                 </button>
             </div>
             <div class="w-1/6 flex justify-center content-start">
-            <button>
+            <button class="response">
                 <p class="md:border md:p-1 rounded-lg hover:bg-white hover:border-gray-400 hover:text-gray-400">Response</p>
             </button>
             </div>
@@ -37,4 +37,112 @@
     </div>
 </div>
 </div>
+{{-- modal --}}
+
+<div class="modal hidden bg-black bg-opacity-50 absolute inset-0 items-center justify-center">
+    <div class="modal md:w-1/4 p-4 bg-white rounded-lg shadow-lg">
+    <div class="flex flex-col">
+        <div class="flex justify-end">
+            <button class="exit">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
+        <p class="flex justify-center my-6 text-lg">What do you response?</p>
+        <div class="flex justify-between">
+            <button class="text-green-500 p-1 px-2 border-green-500 border font-medium rounded-md">
+                Accept
+            </button>
+            <button class="text-red-500 p-1 px-2 border-red-500 border font-medium rounded-md">
+                Reject
+            </button>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="detailmodal hidden bg-black bg-opacity-50 absolute inset-0 items-center justify-center">
+    <div class="w-3/4 md:w-2/5 p-4 bg-white rounded-lg shadow-lg">
+    <div class="flex flex-col">
+        <div class="flex justify-between text-lg my-2">
+            <div class="flex">
+                <p class="font-medium">Id :</p>
+                <p class="ml-3">00001</p>
+            </div>
+            <button class="exitdetail">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
+        <div class="flex justify-between my-2">
+            <div class="flex">
+                <p class="font-medium">Username :</p>
+                <p class="ml-3">Fernando</p>
+            </div>
+            <div class="flex">
+                <p class="font-medium hidden md:inline">Time order :</p>
+                <p class="ml-3">12-12-21, 13.00</p>
+            </div>
+        </div>
+        <p class="text-lg my-2 font-medium">Orders :</p>
+            <div class="flex flex-col w-full my-2">
+                <div class="flex justify-between">
+                    <div class="w-1/6">Espresso</div>
+                    <div class="w-1/6">3x</div>
+                    <div class="w-1/6"><span class="mr-1 hidden md:inline">Rp.</span>7.000</div>
+                    <div class="w-1/6"><span class="mr-1 hidden md:inline">Rp.</span>21.000</div>
+                </div>
+                <div class="flex justify-between">
+                    <div class="w-1/6">Cappucino</div>
+                    <div class="w-1/6">2x</div>
+                    <div class="w-1/6"><span class="mr-1 hidden md:inline">Rp.</span>8.000</div>
+                    <div class="w-1/6"><span class="mr-1 hidden md:inline">Rp.</span>16.000</div>
+                </div>
+                <div class="flex justify-between">
+                    <div class="w-1/6">Arabica</div>
+                    <div class="w-1/6">2x</div>
+                    <div class="w-1/6"><span class="mr-1 hidden md:inline">Rp.</span>7.000</div>
+                    <div class="w-1/6"><span class="mr-1 hidden md:inline">Rp.</span>14.000</div>
+                </div>
+    </div>
+    <div class="flex justify-between text-xl border-t-2">
+        <p>Total :</p>
+        <p>Rp. 51.000</p>
+    </div>
+    </div>
+</div>
+<script>
+    const response = document.querySelector('.response');
+    const exit = document.querySelector('.exit');
+    const modal = document.querySelector('.modal');
+
+    response.addEventListener('click', ()=>{
+            modal.classList.remove('hidden');
+            modal.classList.add('flex');
+        });
+    
+    exit.addEventListener('click', ()=>{
+        modal.classList.remove('flex');
+        modal.classList.add('hidden');
+        detailmodal.classList.remove('flex');
+        detailmodal.classList.add('hidden');
+    });
+
+    const detail = document.querySelector('.detail');
+    const exitdetail = document.querySelector('.exitdetail');
+    const detailmodal = document.querySelector('.detailmodal');
+
+    detail.addEventListener('click', ()=>{
+        detailmodal.classList.remove('hidden');
+        detailmodal.classList.add('flex');
+    })
+
+    exitdetail.addEventListener('click', ()=>{
+        detailmodal.classList.remove('flex');
+        detailmodal.classList.add('hidden');
+    });
+
+</script>
 @endsection
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -8,23 +8,16 @@
     <div class="grid grid-cols-2">
         <img src="/images/{{ $item->image }}" alt="" class="h-40 w-full rounded-l-lg">
         <div class="card-detail bg-white rounded-r-lg px-3 flex flex-col justify-around">
-            <p class="text-xl font-serif text-gray-600">{{ $item->title }}</p>
-            <p class=" text-lg italic">{{ $item->price }}</p>
             <div class="flex justify-between text-gray-600">
-                <div class="flex gap-3">
-                    <button class="minus border-2 border-grey-600"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-                    </svg></button>
-                    <p class="counter">0</p>
-                    <button class="plus border-2 border-grey-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                    </svg>
-                    </button>
-                </div>
+                <p class="text-xl font-serif text-gray-600">{{ $item->title }}</p>
                 <button><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                 </svg></button>
+            </div>
+            <p class=" text-lg italic">{{ $item->price }}</p>
+            <div class="flex ">
+                <label for="">Amount :</label>
+                    <input placeholder="0" type="number" class="w-12 border-2 rounded-md border-gray-600 mx-2 appearance-none text-center">
             </div>
         </div>
     </div>
@@ -32,3 +25,11 @@
 @endforeach 
 </div>
 @endsection
+
+<style>
+    input[type=number]::-webkit-inner-spin-button, 
+input[type=number]::-webkit-outer-spin-button { 
+-webkit-appearance: none; 
+margin: 0; 
+}
+</style>
